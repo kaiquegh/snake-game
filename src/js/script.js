dfc7917fc5250c6936c50dc371da13f83a8b2545
Kaique Heinen <kaique.heinen@neki.com.br>
2021-05-06 20:23:51 -0300

    Customizando a cor da comida, criando função para quando encostrar no corpo acabar o jogo

--- src/js/script.js
+++ src/js/script.js
@@ -24,16 +24,16 @@ function cobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "#e9722c";
+    context.fillRect( food.x, food.y, box, box);
+}
+
 function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
-}
-
-function drawFood(){
-    context.fillStyle = "#e9722c";
-    context.fillRect( food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -43,6 +43,13 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Voce PERDEU!");
+        }
+    }
 
     criarBG();
     cobrinha();
@@ -63,8 +70,6 @@ function iniciarJogo(){
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
 
-    snake.pop();
-
     let newHead = {
         x: snakeX,
         y: snakeY
@@ -74,4 +79,3 @@ function iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
